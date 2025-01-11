3df901a17f9989ad56a8edde33fc08aaa2e9725e
Mohammad <mhamdl2amen@gmail.com>
2025-01-11 16:40:59 +0200

    Update main.py with datetime conversion and plotting improvements

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-
+#set the date format and locators
+year = mdates.YearLocator()
+month = mdates.MonthLocator()
+year_format = mdates.DateFormatter('%Y')
 df_tesla = pd.read_csv('data/TESLA Search Trend vs Price.csv')
 df_btc_search = pd.read_csv('data/Bitcoin Search Trend.csv')
 df_btc_price = pd.read_csv('data/Daily Bitcoin Price.csv')
@@ -28,6 +32,10 @@ ax1.set_ylim([0, 600]) # set y-axis limit
 ax1.set_xlim([df_tesla.MONTH.min(), df_tesla.MONTH.max()]) # set x-axis limit
 ax1.plot(df_tesla.MONTH, df_tesla.TSLA_USD_CLOSE, color='skyblue')
 ax2.plot(df_tesla.MONTH, df_tesla.TSLA_WEB_SEARCH, color='red')
+#set the x-axis major locator and major formatter
+ax1.xaxis.set_major_locator(year)
+ax1.xaxis.set_minor_locator(month)
+ax1.xaxis.set_major_formatter(year_format)
 plt.title('Tesla Web Search vs Price', fontdict=font1)
 plt.savefig('images/Tesla Web Search vs Price.png')
 plt.show()
